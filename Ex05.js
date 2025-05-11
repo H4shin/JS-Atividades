f733b3d50e6e141de2a0c58a2b347f0ecbafc695
Adrian Silva <aueweua@gmail.com>
2025-05-11 12:16:13 -0300

    Commit de alteração no Ex05

--- Ex05.js
+++ Ex05.js
@@ -1,1 +1,14 @@
 //Escreva uma função que receba uma string e retorne quantas vogais (a, e, i, o, u) ela contém.
+function contarVogais(str) {
+  const vogais = 'aeiouAEIOU';
+  let contador = 0;
+
+  for (let char of str) {
+    if (vogais.includes(char)) {
+      contador++;
+    }
+  }
+
+  return contador;
+}
+console.log(contarVogais("Olá, mundo!"));
